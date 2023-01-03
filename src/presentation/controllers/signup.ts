a9d3bae420df72c855cc97512d578c016ef73506
Walany Costa <walanyBnegro@gmail.com>
2023-01-03 17:57:24 +0000

    feat: ensure SignUpController returns 400 if password confirmation fails

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -26,7 +26,14 @@ export class SignupController implements Controller {
                 if (!httpRequest.body[field])
                     return badRequest(new MissingParamError(field));
             }
-
+            if (
+                httpRequest.body.passwordConfirmation !==
+                httpRequest.body.password
+            ) {
+                return badRequest(
+                    new InvalidParamError('passwordConfirmation'),
+                );
+            }
             const isValid = this.emailValidator.isValid(httpRequest.body.email);
 
             if (!isValid) {
