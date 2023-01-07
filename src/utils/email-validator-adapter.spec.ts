a964257eca6300f46a358b01d921988f6c5750b7
Walany Costa <walanyBnegro@gmail.com>
2023-01-07 19:25:43 +0000

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -7,6 +7,10 @@ jest.mock('validator', () => ({
     },
 }));
 
+const makeSut = (): EmailValidatorAdapter => {
+    return new EmailValidatorAdapter();
+};
+
 describe('EmailValidatorAdapter', () => {
     test('should return false if validator returns false', () => {
         const sut = new EmailValidatorAdapter();
@@ -19,5 +23,12 @@ describe('EmailValidatorAdapter', () => {
         const sut = new EmailValidatorAdapter();
         const isValid = sut.isValid('valid_email@mail.com');
         expect(isValid).toBeTruthy();
+    });
+
+    test('should call validator with correct email', () => {
+        const sut = new EmailValidatorAdapter();
+        const isEmailSpy = jest.spyOn(validator, 'isEmail');
+        sut.isValid('any_email@mail.com');
+        expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com');
     });
 });
