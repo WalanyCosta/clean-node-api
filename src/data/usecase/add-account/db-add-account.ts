715255e9b63daef89cbf5225fa11f4a9b5b9662f
Walany Costa <walanyBnegro@gmail.com>
2023-01-11 18:37:30 +0000

    feat: ensure DbAddAccount returns an account on success

--- src/data/usecase/add-account/db-add-account.ts
+++ src/data/usecase/add-account/db-add-account.ts
@@ -1,7 +1,12 @@
 import { AddAccountRepository } from '../../protocols/add-account-repository';
-import { Encrypter, AddAccountModel } from './db-add-account-protocols';
+import {
+    Encrypter,
+    AddAccountModel,
+    AccountModel,
+    AddAccount,
+} from './db-add-account-protocols';
 
-export class DbAddAccount {
+export class DbAddAccount implements AddAccount {
     private readonly encripter: Encrypter;
     private readonly addAccountRepository: AddAccountRepository;
 
@@ -13,12 +18,13 @@ export class DbAddAccount {
         this.addAccountRepository = addAccountRepository;
     }
 
-    async add(account: AddAccountModel): Promise<any> {
-        const hashPassword = await this.encripter.encrypt(account.password);
-        return await this.addAccountRepository.add({
-            name: account.name,
-            email: account.email,
+    async add(accountData: AddAccountModel): Promise<AccountModel> {
+        const hashPassword = await this.encripter.encrypt(accountData.password);
+        const account = await this.addAccountRepository.add({
+            name: accountData.name,
+            email: accountData.email,
             password: hashPassword,
         });
+        return account;
     }
 }
