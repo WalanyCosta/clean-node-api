ffbc9ddd464d4edeb74ff33b5b606931b35df17c
Walany Costa <walanybnegro@gmail.com>
2023-03-16 11:21:32 +0100

    chore: remove mongodb driver

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   coverageDirectory: 'coverage',
   collectCoverageFrom:['<rootDir>/src/**/*.ts'],
   testEnvironment: 'jest-environment-node',
-  preset: '@shelf/jest-mongodb',
   transform: {
       '.+\\.ts$': 'ts-jest',
   }
