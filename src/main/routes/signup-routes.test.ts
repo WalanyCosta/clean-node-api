e2275675d12fc5813593f597a28ccd05d5f31166
Walany Costa <walanybnegro@gmail.com>
2023-03-17 23:35:28 +0100

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,19 @@
 import request from 'supertest';
 import app from '../config/app';
+import * as MongooseHelper from '../../infra/db/mongoose/helpers/mongoose-helper';
 
 describe('CORS Middleware', () => {
+    beforeAll(async () => {
+        await MongooseHelper.connect();
+    });
+
+    afterAll(async () => {
+        await MongooseHelper.disconnect();
+    });
+
+    beforeEach(async () => {
+        await MongooseHelper.cleanData();
+    });
     test('should return an account on success', async () => {
         await request(app)
             .post('/api/signup')
