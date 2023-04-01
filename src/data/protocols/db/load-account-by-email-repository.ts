1f299ab027403b5c2153544cded95aba55ac97ee
Walany Costa <walanybnegro@gmail.com>
2023-04-01 12:45:18 +0100

    feat: ensure DbAuthentication calls HashComparer with correct values

--- src/data/protocols/db/load-account-by-email-repository.ts
+++ src/data/protocols/db/load-account-by-email-repository.ts
@@ -1,4 +1,4 @@
-import { AccountModel } from '../usecase/add-account/db-add-account-protocols';
+import { AccountModel } from '../../../domain/models/account-model';
 
 export interface LoadAccountByEmailRepository {
     load: (email: string) => Promise<AccountModel>;
