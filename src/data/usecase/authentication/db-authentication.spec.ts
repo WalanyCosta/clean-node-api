322f51bdb9a992b182385af99f60e5def35e48e2
Walany Costa <walanybnegro@gmail.com>
2023-04-04 16:58:14 +0100

    refactor: move DbAuthentication protocols to a single file

--- src/data/usecase/authentication/db-authentication.spec.ts
+++ src/data/usecase/authentication/db-authentication.spec.ts
@@ -1,9 +1,11 @@
-import { AuthenticationModel } from '../../../domain/usecases/authentication';
-import { HashComparer } from '../../protocols/criptography/hash-comparer';
-import { TokenGenerator } from '../../protocols/criptography/token-generator';
-import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
-import { UpdateAccessTokenGenerator } from '../../protocols/db/update-access-token-generator';
-import { AccountModel } from '../add-account/db-add-account-protocols';
+import {
+    AuthenticationModel,
+    HashComparer,
+    TokenGenerator,
+    LoadAccountByEmailRepository,
+    UpdateAccessTokenGenerator,
+    AccountModel,
+} from './db-authentication-protocols';
 import { DbAuthentication } from './db-authentication';
 
 const makeLoadAccountByEmailRepository = (): LoadAccountByEmailRepository => {
--- src/data/usecase/authentication/db-authentication.ts
+++ src/data/usecase/authentication/db-authentication.ts
@@ -1,11 +1,11 @@
 import {
-    Authentication,
     AuthenticationModel,
-} from '../../../domain/usecases/authentication';
-import { HashComparer } from '../../protocols/criptography/hash-comparer';
-import { TokenGenerator } from '../../protocols/criptography/token-generator';
-import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
-import { UpdateAccessTokenGenerator } from '../../protocols/db/update-access-token-generator';
+    Authentication,
+    HashComparer,
+    TokenGenerator,
+    LoadAccountByEmailRepository,
+    UpdateAccessTokenGenerator,
+} from './db-authentication-protocols';
 
 export class DbAuthentication implements Authentication {
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
