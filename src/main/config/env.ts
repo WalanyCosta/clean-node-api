eeb59782c19286bde7ae9ad6cfc65dc7b572d751
Walany Costa <walanybnegro@gmail.com>
2023-04-07 03:09:10 +0100

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,4 +2,5 @@ export default {
     mongoUrl:
         process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/clean-node-api',
     port: process.env.PORT || '3333',
+    jwtSecret: process.env.JWT_SECRET || 'tj670==5H',
 };
