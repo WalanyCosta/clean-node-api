b1515f87a7a6ba839ba0401d2af00ef9d2637b61
Walany Costa <walanybnegro@gmail.com>
2023-04-14 01:52:05 +0100

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,8 @@
 import request from 'supertest';
 import app from '../config/app';
 import * as MongooseHelper from '../../infra/db/mongoose/helpers/mongoose-helper';
+import { AccountMongoose } from '../../infra/db/mongoose/model/account-mongo-model';
+import { hash } from 'bcrypt';
 
 describe('Login Routes', () => {
     beforeAll(async () => {
@@ -24,6 +26,25 @@ describe('Login Routes', () => {
                     email: 'any_email@mail.com',
                     password: '123',
                     passwordConfirmation: '123',
+                })
+                .expect(200);
+        });
+    });
+
+    describe('Post /login', () => {
+        test('should return 200 on login', async () => {
+            const password = await hash('123', 12);
+            await new AccountMongoose({
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                password,
+            }).save();
+
+            await request(app)
+                .post('/api/login')
+                .send({
+                    email: 'any_email@mail.com',
+                    password: '123',
                 })
                 .expect(200);
         });
