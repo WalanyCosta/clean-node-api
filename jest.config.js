868501d9d4ab2b2aa36b69ee9f4db523ada42939
Walany Costa <walanybnegro@gmail.com>
2023-04-14 02:18:03 +0100

    refactor: ignore file jest to not be found coverage

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,9 @@ module.exports = {
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**/*',
     '!<rootDir>/src/presentation/protocols/index.ts',
-    '!<rootDir>/src/presentation/controllers/signup/signup-protocols.ts',
+    '!<rootDir>/src/presentation/controllers/signup/signup-controller-protocols.ts',
+    '!<rootDir>/src/presentation/controllers/login/login-controller-protocols.ts',
+    '!<rootDir>/src/data/usecase/authentication/db-authentication-protocols.ts',
   ],
   testEnvironment: 'jest-environment-node',
   transform: {
